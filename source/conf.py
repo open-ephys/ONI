9ff454cdc6300acb87f953b4c211cea2db59f333
bparks13 <bap@open-ephys.org>
2024-04-25 10:04:24 -0400

    Ignore build warnings for missing xrefs in breathe/sphinx_csharp

--- source/conf.py
+++ source/conf.py
@@ -49,6 +49,14 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
+# Do not create cross references for these standard/build-in types
+sphinx_csharp_ignore_xref = [
+  'T',
+  'IntPtr',
+  'SafeHandleZeroOrMinusOneIsInvalid',
+  'Exception'
+]
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
